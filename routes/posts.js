7ca8de015c4f3213e476df12e79938067c18690a
unknown <tlu37317@gmail.com>
2014-11-23 11:32:46 +0800

    add posts.js a init data, #4

--- routes/posts.js
+++ routes/posts.js
@@ -1,4 +1,16 @@
-exports.list = function(req, res){
+var post = 
+[{
+    "title": "您好",
+    "content": "今天是星期天"
+},
+{
+    "title": "Hello",
+    "content": "Today is sunday"
+}];
+
+
+exports.list = function(req, res){   // express預設一定有request & response函數
+    res.send(post);
 };
 
 exports.create = function(req, res){
