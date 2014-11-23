e100f32aedd381594e50cc18d23f8351c76c10d9
unknown <tlu37317@gmail.com>
2014-11-23 11:58:22 +0800

    add http.listen.port and fix express flow, close #4

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var http = require('http');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
 var posts = require('./routes/posts');  //1123 class morning add
 
 var app = express();
@@ -26,6 +26,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+//1123 class morning add (express middleware的關係  改到這裡才會work  不然會先跳404)
+app.get('/1/post', posts.list);
+app.post('/1/post', posts.create);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -58,8 +62,9 @@ app.use(function(err, req, res, next) {
     });
 });
 
-//1123 class morning add
-app.get('/1/post', posts.list);
-app.post('/1/post', posts.create);
+http.createServer(app).listen(3000, function(){
+    console.log('Express server lisening on port 3000');
+});
+
 
 module.exports = app;
