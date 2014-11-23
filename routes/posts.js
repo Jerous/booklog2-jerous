51bbebb2769c88d5bd3af9141def11a356a2c9c2
unknown <tlu37317@gmail.com>
2014-11-23 13:54:06 +0800

    set mongoose in express, #2

--- routes/posts.js
+++ routes/posts.js
@@ -1,16 +1,15 @@
-var post = 
-[{
-    "title": "您好",
-    "content": "今天是星期天"
-},
-{
-    "title": "Hello",
-    "content": "Today is sunday"
-}];
-
 
 exports.list = function(req, res){   // express預設一定有request & response函數
-    res.send(post);
+    var model = req.app.db.model.Post;
+
+    model
+      .find({})
+      .exec(function(err, posts) {  //exec表示以上條件成功後執行後面Funtion
+          res.send({
+              posts: posts
+          });
+          res.end();
+      });
 };
 
 exports.create = function(req, res){
